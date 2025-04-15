1a7c4a72f28f9c15f6401545b63ec7a18668e857
tamije24 <tamije24@gmail.com>
2025-04-15 14:59:06 +0530

    Checking Deployment

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+// Change baseURL to the correct backend URL
 const apiClient = axios.create({
    //baseURL: 'http://127.0.0.1:8000/',
    baseURL: 'https://signal-analyser-prod-9dcd276c75aa.herokuapp.com/',
